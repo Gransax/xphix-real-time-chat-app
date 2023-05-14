127934233c46ba9150fa6f63b3a2782fcd770bdd
xphix <chouelakevin@gmail.com>
2023-05-14 18:56:57 +0100

    v0.3

--- app/hooks/useConversation.ts
+++ app/hooks/useConversation.ts
@@ -6,7 +6,9 @@ const useConversation = () => {
   const conversationId = useMemo(() => {
     if (!params?.conversationId) return "";
 
-    return params.conversationId as string;
+    const cId: string = params.conversationId as string;
+
+    return cId;
   }, [params?.conversationId]);
 
   const isOpen = useMemo(() => !!conversationId, [conversationId]);
